b4a0985fb2408a04f0bb393dd71bc4779b5b77ce
Aakarsh Nair <aknair@ucsc.edu>
2016-05-08 04:12:18 -0700

    sorted list of worst mis predictions

--- src/scripts/summary.py
+++ src/scripts/summary.py
@@ -9,6 +9,7 @@ def print_summary():
     " Print the summary of misprediction results  "
     path = "/var/host/media/removable/My Passport/src/results/MYRESULTS"
     file_list = glob.glob(path+"/*.res")
+    trace_time = {}
     for f in file_list:
         filename = os.path.basename(f)
         fo = open(f)
@@ -18,9 +19,12 @@ def print_summary():
         results ={}
         for (v1,v2,v3) in grouper(3,values):
             results[v1] = v3
-
+        
         if "MISPRED_PER_1K_INST" in results and len(results) > 0:
-            print filename,":",results["MISPRED_PER_1K_INST"]
+            trace_time[filename] = float(results["MISPRED_PER_1K_INST"])
+
+    for key in sorted(trace_time,key=trace_time.get,reverse=True):
+        print key,"->",trace_time[key]
 
 
 def grouper(n,iterable,fill_value = None):
